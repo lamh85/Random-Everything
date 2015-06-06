42f5eb036848c9f20e90df2065138ff51cb7178d
Hon Lam <lamh85@gmail.com>
2015-06-06 16:28:25 -0700

    Refactored the div-creation code

--- script.js
+++ script.js
@@ -1,65 +1,38 @@
-var squares = [];
-var squareCss = {};
-var ScreenWidth = $('body').width;
-var ScreenHeight = $('body').height;
-
-console.log("ScreenWidth is " +ScreenWidth);
-console.log("ScreenHeight is " +ScreenHeight);
-
-
-var makeSquareCss = function(red,green,blue,width,height,radius,left,top) {
+var makeSquareCss = function(red,green,blue,opacity,width,height,radius,rotate,left,top) {
   return {
     background: "rgb(" +red+ "," +green+ "," +blue+ ")",
-    height: height+ "px",
+    opacity: opacity,
     width: width+ "px",
+    height: height+ "px",
     borderRadius: radius+ "%",
+    transform: "rotate(" +rotate+ "deg)",
     position: "absolute",
     left: left+ "%",
     top: top+ "%"
   }
 }
 
-
-
 var makeRandom = function(limit) {
   return Math.round(limit * Math.random());
 }
 
-for (i = 1; i < ( makeRandom(50)+50 ); i++ ) {
-  squareCss = makeSquareCss(
+var makeSquareCssRandom =  function(){
+  return makeSquareCss(
     makeRandom(255), // red
     makeRandom(255), // green
     makeRandom(255), // blue
-    makeRandom(200), // width
-    makeRandom(200), // height
+    Math.random(), // opacity
+    makeRandom(300), // width
+    makeRandom(300), // height
     makeRandom(100), // border-radius
+    makeRandom(360), // rotate
     makeRandom(100), // left
     makeRandom(100) // top
-  )
-  $('div.clone-me:last').clone().css(squareCss).appendTo('body');
+  );
 }
 
-var renderSquares = function(){
-  for (i = 0; i < squares.length; i ++) {
-    $('body').append( $('div').css(squares[i]) );
-  }
+for (i = 1; i < ( makeRandom(50)+50 ); i++ ) {
+  $('div.clone-me:last').clone().css( makeSquareCssRandom() ).addClass('duplicate').removeClass('clone-me').animate(
+      makeSquareCssRandom(), makeRandom(10000) // Create random CSS and random duration, max 10 seconds
+    ).appendTo('body');
 }
-
-
-
-// How to style using objects:
-// ///////////////////////////
-// $( "p" )
-//   .on( "mouseenter", function() {
-//     $( this ).css({
-//       "background-color": "yellow",
-//       "font-weight": "bolder"
-//     });
-//   })
-//   .on( "mouseleave", function() {
-//     var styles = {
-//       backgroundColor : "#ddd",
-//       fontWeight: ""
-//     };
-//     $( this ).css( styles );
-//   });
